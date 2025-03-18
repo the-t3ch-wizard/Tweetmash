3f4e820ef1157b23e0d9a0e06207fe37a9eaf7cb
ayush <ayushmaurya2050@gmail.com>
2025-03-19 05:11:03 +0530

    Revert "FIX: server's start command"
    This reverts commit 4fda49d253c304bf456928e6ffbf3a1da7191968.

--- server/src/middlewares/logger.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loggerMiddleware = void 0;
-const logger_1 = require("../lib/logger");
-const loggerMiddleware = (req, res, next) => {
-    logger_1.logger.info(`REQUEST: ${req.method} ${req.url}`);
-    next();
-};
-exports.loggerMiddleware = loggerMiddleware;
--- server/src/routes/user/user.routes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoutes = void 0;
-const express_1 = require("express");
-exports.userRoutes = (0, express_1.Router)();
